8dbd94cf87b2d8c52be36b1cadfd7add6a9ef3af
Obi1anzor <shindin112@gmail.com>
2024-05-03 18:08:48 +0300

    update question style

---
+++ PresentationLayer/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models;
+using System.Diagnostics;
+
+using BusinessLogicLayer.Services.Contracts;
+using DataAccessLayer.Models;
+
+namespace PresentationLayer.Controllers
+{
+    public class GameController : Controller
+    {
+        [HttpPost]
+        public IActionResult CheckAnswer(string selectedAnswer)
+        {
+            // Проведите проверку правильности ответа
+            bool isCorrect = true; // Здесь PerformAnswerCheck - ваша логика проверки ответа
+
+            // Верните результат проверки обратно на клиент
+            return Json(new { isCorrect = isCorrect });
+        }
+    }
+}
