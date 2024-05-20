5ebc9d818e0c669287d0f5b56aae24b790f0644c
Wasd-Qwerty <112080391+obi1anzor075@users.noreply.github.com>
2024-05-20 11:30:54 +0300

    update

--- DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2f57d2a36bb569c2f0842b19a3992c61447ebb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+710d2cd6fadc50367775c56368a872c8a5386a96")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
