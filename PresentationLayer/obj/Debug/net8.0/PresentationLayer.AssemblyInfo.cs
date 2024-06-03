8dd6a420ca8fffc4d6896f137a9cb448bfef82fd
Areki <shindin112@gmail.com>
2024-06-03 13:38:42 +0300

    add duel connection room

--- PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
+++ PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6eedfd5226e76d23ec9336ef5a582949775fab49")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bdff7f5f1f4e7ea54f3a735a1fb2dc0d19a0f6dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
