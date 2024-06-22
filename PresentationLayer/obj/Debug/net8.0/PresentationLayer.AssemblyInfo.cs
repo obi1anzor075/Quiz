34b79c786aca4374e39ca398ab705d6ad2a36db9
obi1anzor075 <shindin112@gmail.com>
2024-06-22 22:56:48 +0300

    up

--- PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
+++ PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3022d8bf460b72b94fe418e39f32ce039687b76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e240bc645ef021a2084f816162707b5a16214a2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
