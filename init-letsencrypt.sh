14f9d1f2178f811ce6244bc7637a5f7164bc9bc9
obi1anzor075 <shindin112@gmail.com>
2024-07-06 19:19:08 +0300

    up

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 domains=(brand-battles.ru)
-email="shindin112@mail.ru"
+email="shindin112@mail.ru" # Введите свой email здесь
 rsa_key_size=4096
 data_path="./data/certbot"
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=0 # Установите 1, если вы тестируете скрипт, чтобы избежать ограничения на количество запросов
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
